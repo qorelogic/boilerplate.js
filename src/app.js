454051f756d8c3e3074534b590374949f1a64ea2
Jeff Kiiza <kiizaa@gmail.com>
2016-10-08 17:11:53 -0300

    mod app.js webpack mod cats/dogs

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
-
-cats = require('./cats.js')
+import cats from './cats'; // previously[without webpack]: cats = require('./cats.js')
+import dogs from './dogs';
 console.log(cats);
+console.log(dogs);
---
+++ src/dogs.js
@@ -0,0 +1,3 @@
+
+var dogs = ['dave2', 'henry2', 'martha2']
+module.exports = dogs
