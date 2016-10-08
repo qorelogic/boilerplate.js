1f084236adab0e2ee485947135f9dc913d9b217a
Jeff Kiiza <kiizaa@gmail.com>
2016-10-08 14:12:32 -0300

    - install  - packacke.json

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 
+if [ ! -f package.json ]; then
+echo 'npm init..'
+npm init
+fi
+
+if [ -f package.json ]; then
+echo 'installing webpack..'
+npm install --save-dev webpack
+
+echo 'installing babel..'
 npm install --save-dev babel-core babel-preset-es2015
 npm install --save-dev babel-loader
+fi
