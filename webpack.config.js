0c8aeecf5aaddd5b10ea90eb78fe93975fd1641f
Jeff Kiiza <kiizaa@gmail.com>
2016-10-08 17:13:20 -0300

    - npm run babel  - npm run webpack  - presets es2015, react

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,12 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
     }]
   }
 };
