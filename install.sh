54ddb26eaf72b4cf58980867ea8180cc4f4a9f12
Jeff Kiiza <kiizaa@gmail.com>
2016-10-08 18:47:04 -0300

    - npm install --save-dev react  - npm install --save-dev react-dom

--- install.sh
+++ install.sh
@@ -12,4 +12,8 @@ echo 'installing babel..'
 npm install --save-dev babel-core babel-preset-es2015
 npm install --save-dev babel-loader
 npm install --save-dev babel-preset-react
+
+echo 'installing react..'
+npm install --save-dev react
+npm install --save-dev react-dom
 fi
